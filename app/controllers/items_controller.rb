7c8e9c8a1be9e0baec6a4891cb30b41ab291e8e9
Kelly Duran <kaduran16@gmail.com>
2022-09-03 16:29:16 -0400

    commit tree 304d6581d946b8f12aaec3b397ebbbfce15df799

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items, except: [:created_at, :updated_at] 
+    else
+      items = Item.all
+      render json: items, except: [:created_at, :updated_at], include: :user
+    end
   end
+
+
+  def show
+    items = Item.find(params[:id])
+    render json: items, except: [:created_at, :updated_at] 
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, status: :created
+  end
+  
+  
+  private
+  def render_not_found_response
+     render json: {error: "Not found"}, status: :not_found 
+  end
+  
+  def item_params
+      params.permit(:name, :description, :price, :user_id)
+    end
+
 
 end
